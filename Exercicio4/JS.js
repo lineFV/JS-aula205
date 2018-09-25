44279ddd02fbb51a34ddf9d499012177bbee5698
lineFV <aline.fer.val@gmail.com>
2018-09-25 11:54:40 -0300

    exercicios alterados

--- Exercicio4/JS.js
+++ Exercicio4/JS.js
@@ -1,19 +1,16 @@
-let meninos = ["Alfredo", "Pedro", "Anastacio", "Waldisney"];
+let meninos = ["Alfredo", "Pedro", "Anastacio", "Waldisney",];
 let meninas = ["Alexandrina", "Francisca", "Josefa", "Marinalva"];
 let pares = meninos.length * meninas.length;
 let i = 0;
-let o = 0;
 //let u = 0;
-let boy = meninos[i];
 
 while (i < meninos.length) {
-    console.log(i);
-    console.log(meninos[i])
-    i++;
+    let o = 0;
     while (o < meninas.length) {
-       // console.log(`${boy} + ${meninas[o]}`)
+       console.log(`Casal:${meninos[i]} + ${meninas[o]}`)
         o++;
 
     }
+    i++;
 }
 console.log(`${pares} casais`);
